60aaf40078e2f9b74286ae2563b2766a9a948b8f
reece-elder <reece.elder@qa.com>
2021-11-02 11:33:55 +0000

    objects json

---
+++ JavaScriptRecap/005-Objects-JSON/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// Objects within JS
+// Objects are collections of data, used for transferring info?
+// JS Objects are not physical like they are in Java 
+
+let zaph = {
+    name: "Zaph",
+    colour: "Ashy Black",
+    fluffy: true,
+    goodCatRating: 9,
+    chonky: true
+};
+
+// Objects are PURELY just a data type
+
+console.log(zaph);
+console.log(zaph.colour);
+
+// A getter for the object 
+function getColour(){
+    return zaph.colour
+};
+
+console.log(getColour);
+
+zaph.colour = "Ashy Brown";
+console.log(zaph);
+zaph.legs = 4;
+console.log(zaph);
+
+// Arrays can store objects
+
+let cattery = [
+    {name: "Oliver", colour: "Cinnamon", fluffy: false, goodCatRating: 7, chonky: true},
+    {name: "Hazel", colour: "Beige Fawn", fluffy: true, goodCatRating: 8, chonky: false},
+    {name: "Oliver2", colour: "Cinnamon", fluffy: false, goodCatRating: 7, chonky: true},
+    {name: "Hazel2", colour: "Beige Fawn", fluffy: true, goodCatRating: 8, chonky: false}
+];
+
+console.log(cattery); // Within Java would print wekfewfb727@cattery
+// Print out the colour of our first cat
+
+console.log(cattery[0].colour);
