7543a17845e14b68dc292ddb8cc559a6420fe698
reece-elder <reece.elder@qa.com>
2021-11-03 10:16:53 +0000

    query selector all

--- JavaScriptRecap/008-DOM-Manipulation/index.js
+++ JavaScriptRecap/008-DOM-Manipulation/index.js
@@ -12,3 +12,15 @@ console.log(typeof header1);
 header1.textContent = "This is new text.. wild";
 
 header1.style.color = 'red';
+
+// To GRAB all elements of a DOM rather than the first use querySelectorAll
+let listItemList = document.querySelectorAll('li');
+console.log(listItemList);
+
+listItemList[1].style.border = '5px solid red';
+
+// How can I give all of the items in my list a purple border??
+// A foreach loop for my list of elements, give them all a purple border
+// for(let li of listItemList){
+//     li.style.border = '5px solid purple';
+// };
